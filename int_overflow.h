003c949772b9199e3a654bb043bb88b96a57cd51
ansao-aci <ansao@cisco.com>
2021-10-14 14:12:14 +0530

    Least number  of sqaures to get a sum of n , two solitions: 1. dp 2. dp+bfs.

--- int_overflow.h
+++ int_overflow.h
@@ -50,3 +50,108 @@ int int_overflow::reverse(int a){
     return res;
 
 }
+
+/*
+Return least number of sqaures needed to make sum of n.
+12  = 4+4+4  < 9+1+1+1   == 3
+11  = 9+1+1  = 2
+10  = 9+1 = 2
+
+axiom : n is positive integer.
+Solution: 
+  This problem can be solved by dynamic programming. 
+  It has optimal substructure and overlapping subproblems. 
+  dp[12] = min (1 + dp[3], 1+ dp[9], 1 + dp[11])  (1,4,9)
+  dp[10] = min(1+dp[6], 1+dp[9], 1+dp[1])
+  
+  for all j , where j*j <= i
+  dp[i] = min (dp[i], 1 + dp[i-j*j])
+
+  using above recurrence relation, calculate and return dp[n]
+
+  Solution1 time taken is 12ms although algorithm is same. 
+  Used static vector.
+  using above recurrence relation, calculate and return dp[n].
+  If we use non-static vector for memoization, time taken shoots up to ~200ms.
+  If we make it static, it is down to ~10ms. Static vector preserves its state across 
+  function calls hence we can reuse results computed in earlier function calls.
+
+
+Solution using bfs:
+put all square upto n in VecS;
+put all sqaure upto n in Q;
+
+till Q is empty, combine every element in Q with VecS
+and create new elements, count of sqaure depends upon number of pops 
+done from Q.
+if the new element is n, return count.
+if new element is < n, preserve count and push element to Q.
+if new element is > n, ignore this and any elements later this. 
+
+
+*/
+
+class leastSquareSum{
+    public:
+    int solution1(int n);
+    int solution2(int n);
+};
+int leastSquareSum::solution1(int n){
+    int m = sqrt(n);
+    if (m*m == n) return 1;
+        
+    static vector<int> dp(10001,INT_MAX);  dp[0] = 0; dp[1] = 1;
+    if (dp[n]!= INT_MAX) return dp[n];
+
+    for(int i=2;i<=n;i++){
+        for(int  j=1;j*j<=i;j++) {
+            dp[i] = min(dp[i], 1+ dp[i-j*j]);
+        }
+    }
+    return dp[n];
+}
+
+int leastSquareSum::solution2(int n){
+    vector<int> perfectSquares;
+    vector<int> cntPerfectSquares(n+1);
+        
+    for (int i = 1; i*i <= n; i++)
+    {
+        perfectSquares.push_back(i*i);
+        cntPerfectSquares[i*i] = 1;
+    }
+        
+    if (perfectSquares.back() == n)
+    {
+        return 1;
+    }
+        
+    queue<int> searchQ;
+    for (auto& i : perfectSquares)
+    {
+        searchQ.push(i);
+    }
+        
+    int currCntPerfectSquares = 1;
+    while (!searchQ.empty())
+    {
+        currCntPerfectSquares++;
+        for (int i = 0; i < searchQ.size(); i++){
+            int tmp = searchQ.front();
+            for (auto& j : perfectSquares){
+                if (tmp + j == n){
+                    return currCntPerfectSquares;
+                }
+                else if ((tmp + j < n) && (cntPerfectSquares[tmp + j] == 0)){
+                    cntPerfectSquares[tmp + j] = currCntPerfectSquares;
+                    searchQ.push(tmp + j);
+                }
+                else{
+                    break;
+                }
+            }
+            searchQ.pop();
+        }
+    }        
+    return 0;
+}
